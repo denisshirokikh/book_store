9cbdae0e33dcd50edaef6fd3d3b3db1431ade583
Denis Shirokikh <denisshirokikh@gmail.com>
2020-10-18 14:32:31 -0700

    added classes

---
+++ main.rb
@@ -0,0 +1,19 @@
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative 'lib/product'
+require_relative 'lib/book'
+require_relative 'lib/movie'
+
+leon = Movie.new(price: 290, amount: 100)
+
+puts "Фильм Леон стоит #{leon.price} руб."
+
+
+
